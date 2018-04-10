f6511c0c81a1290eea1b7ac91f52042a08aaf17a
em1lbuS <rodolfoandresm@gmail.com>
2018-04-09 21:40:52 -0300

    Array en 2D que simula el tablero. Más bonito así.

--- matrix.c
+++ matrix.c
@@ -2,46 +2,46 @@
 
 int ingresarJugada(){
 
-   /* Un array con "12" filas y "9" columnas.*/
-   /* El inicio del tablero está dado por el "8" y el final por el "9".*/
-   int tablero[12][9] = {  {0,0,0,0,9,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0,0},
-                     {0,0,0,0,8,0,0,0,0}  };
+    /* Un array con "12" filas y "9" columnas.*/
+    /* El inicio del tablero está dado por el "8" y el final por el "9".*/
+    int tablero[12][9] = {  {0,0,0,0,9,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,0,0,0,0,0},
+                            {0,0,0,0,8,0,0,0,0}  };
 
-   int fila;
-   int columna;
-   printf("\n");
-   for ( fila = 0; fila < 12; fila++ ) {
+    int fila;
+    int columna;
+    printf("\n");
+    for ( fila = 0; fila < 12; fila++ ) {
 
-      for ( columna = 0; columna < 9; columna++ ) {
+        for ( columna = 0; columna < 9; columna++ ) {
 
-         if(columna == 8){
+            if(columna == 8){
 
             printf("%d\n", tablero[fila][columna]);
-         }
-         
-         else
+            }
+
+            else
             printf("%d", tablero[fila][columna]);
-      }
-   }
-   
-   return 0;
+        }
+    }
+
+    return 0;
 }
 
 int main () {
 
-   ingresarJugada();
+    ingresarJugada();
 
-   return 0;
+    return 0;
 }
 
 
