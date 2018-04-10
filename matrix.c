b379fc9a2725c22a660bdc5e69ae12daa59c736e
em1lbuS <rodolfoandresm@gmail.com>
2018-04-09 21:31:39 -0300

    Array en 2D hecha arcaicamente que debería simular el tablero del juego, cuyo comienzo es simbolizado por un 8 y fin por un 9.

---
+++ matrix.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int ingresarJugada(){
+
+   /* Un array con "12" filas y "9" columnas.*/
+   /* El inicio del tablero está dado por el "8" y el final por el "9".*/
+   int tablero[12][9] = {  {0,0,0,0,9,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,0,0,0,0,0},
+                     {0,0,0,0,8,0,0,0,0}  };
+
+   int fila;
+   int columna;
+   printf("\n");
+   for ( fila = 0; fila < 12; fila++ ) {
+
+      for ( columna = 0; columna < 9; columna++ ) {
+
+         if(columna == 8){
+
+            printf("%d\n", tablero[fila][columna]);
+         }
+         
+         else
+            printf("%d", tablero[fila][columna]);
+      }
+   }
+   
+   return 0;
+}
+
+int main () {
+
+   ingresarJugada();
+
+   return 0;
+}
+
+
+
+
