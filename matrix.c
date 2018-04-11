9bfb4779206e12d73de0505e4ba81fb8c4a5303a
em1lbus <rodolfoandresm@gmail.com>
2018-04-11 13:28:28 -0300

    Se agregaron ciertas funcionalidades a funciones propiamente tal, pero aun existe error de puntero invalido.

--- matrix.c
+++ matrix.c
@@ -1,21 +1,66 @@
 #include <stdio.h>
 
-int ingresarJugada(){
+int ** generarTablero(){
 
-    /* Un array con "12" filas y "9" columnas.*/
-    /* El inicio del tablero está dado por el "8" y el final por el "9".*/
-    int tablero[12][9] = {  {0,0,0,0,9,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,0,0,0,0,0},
-                            {0,0,0,0,8,0,0,0,0}  };
+    /*  Función que genera un array con "12" filas y "9" columnas, asemejando una matriz que a su vez asemeja un tablero de juego.
+        El inicio del tablero está dado por el "8" y el final por el "9".
+        Números "2" simbolizan casilleros inválidos. */
+
+    static int tablero[12][9] = {  {2,2,2,2,9,2,2,2,2},
+                                {0,0,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,0,0},
+                                {0,0,0,0,0,0,0,0,0},
+                                {2,2,2,2,8,2,2,2,2}  };
+
+    return tablero;
+
+}
+
+int ** ingresarJugada(int ** tableroModificado){
+    
+    int jugada;
+    printf("Ingrese su jugada:\n>>");
+    scanf("%d", &jugada);
+
+    int j = 4;
+    int i = 11;
+
+    if(jugada == 1)
+        tableroModificado[i - 1][j - 1] = 1;
+
+    if(jugada == 2)
+        tableroModificado[i - 1][j] = 1;
+
+    if(jugada == 3)
+        tableroModificado[i - 1][j + 1] = 1;
+
+    if(jugada == 4)
+        tableroModificado[i][j + 1] = 1;
+
+    if(jugada == 5)
+        tableroModificado[i + 1][j + 1] = 1;
+
+    if(jugada == 6)
+        tableroModificado[i + 1][j] = 1;
+
+    if(jugada == 7)
+        tableroModificado[i + 1][j - 1] = 1;
+
+    if(jugada == 8)
+        tableroModificado[i][j - 1] = 1;
+
+    return tableroModificado;
+
+} 
+
+int imprimirTablero(int ** tablero){
 
     int fila;
     int columna;
@@ -37,12 +82,16 @@ int ingresarJugada(){
     return 0;
 }
 
-int main () {
 
-    ingresarJugada();
+int main(){
+
+    int ** tablero = generarTablero();
+    imprimirTablero(tablero);
+    
 
     return 0;
 }
+
 
 
 
